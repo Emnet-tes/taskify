c3ba697290462dacaea534ecbb9045a302b146b2
Emnet-tes <teshomeemnet255@gmail.com>
2025-02-14 18:19:45 +0300

    add modal

---
+++ app/components/Modal.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { TaskProps } from "../types";
+import { IoIosCloseCircle } from "react-icons/io";
+
+interface ModalProps {
+  isModalOpen: boolean;
+  onClose: () => void;
+  task: TaskProps;
+  editTask: (index: number, task: TaskProps) => void;
+  index: number;
+}
+
+const Modal = ({ isModalOpen, onClose, task, editTask, index }: ModalProps) => {
+  const [newTask, setNewTask] = React.useState<TaskProps>(task);
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    editTask(index, newTask);
+    onClose();
+  }
+
+  if (!isModalOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex justify-center items-center backdrop-blur-md bg-black bg-opacity-50"
+      onClick={onClose} // Closes modal when clicking outside
+    >
+      <div
+        className="relative w-full max-w-lg p-8 rounded-3xl shadow-xl bg-white"
+        onClick={(e) => e.stopPropagation()} // Prevents closing when clicking inside
+      >
+        {/* Close Button */}
+        <button
+          className="absolute top-3 right-2 text-gray-600 hover:text-red-500 transition"
+          onClick={onClose}
+        >
+          <IoIosCloseCircle size={28} />
+        </button>
+
+        <form onSubmit={handleSubmit}>
+          <div className="flex gap-2 items-center">
+            <input
+              type="text"
+              placeholder="Edit task"
+              className="w-full border-2 border-gray-300 rounded p-2"
+              value={newTask.value}
+              onChange={(e) =>
+                setNewTask({ value: e.target.value, status: newTask.status })
+              }
+              autoFocus
+            />
+            <button
+              type="submit"
+              className="p-3 bg-indigo-600 text-white rounded text-sm"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import { TaskProps } from "../types";
 import { IoIosCheckbox } from "react-icons/io";
 import { MdCheckBoxOutlineBlank } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
+import Modal from "./Modal";
 interface TaskCardProps {
   task: TaskProps;
   index: number;
@@ -17,6 +19,10 @@ const TaskCard: React.FC<TaskCardProps> = ({
   editTask,
   deleteTask,
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  }
   return (
     <li className="list-group-item flex justify-between border-2 border-gray-200 p-4 mb-2 rounded-lg ">
       <div className="flex items-center gap-2">
@@ -39,12 +45,7 @@ const TaskCard: React.FC<TaskCardProps> = ({
       </div>
       <div className="flex gap-6">
         <button
-          onClick={() => {
-            const newValue = prompt("Edit task", task.value);
-            if (newValue !== null) {
-              editTask(index, { ...task, value: newValue });
-            }
-          }}
+          onClick={handleModalToggle} 
         >
           {" "}
           <FaEdit />
@@ -54,6 +55,13 @@ const TaskCard: React.FC<TaskCardProps> = ({
           <MdDelete />
         </button>
       </div>
+      {isModalOpen && (
+          <div onClick={handleModalToggle} >
+            <div className="relative bg-white p-6 rounded-lg shadow-lg max-w-md w-full " onClick={(e) => e.stopPropagation}>
+              <Modal isModalOpen={isModalOpen} onClose={handleModalToggle} task={task} editTask={editTask} index={index} />
+            </div>
+          </div>
+      )}
     </li>
   );
 };
